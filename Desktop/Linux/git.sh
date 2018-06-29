aa5d2ac3bd6b66dfbe3c01c2327526d5b6648391
ZJ <ZJ@asssi.local>
2018-06-29 11:37:45 +0800

    ww

--- Desktop/Linux/git.sh
+++ Desktop/Linux/git.sh
@@ -1,9 +1,9 @@
-# read -p "请输入项目地址:" address
-# git add "$address"
+read -p "请输入项目地址:" address
+git add $address
 
-# echo $address
+echo $address
 # echo $1
-git add "$1"
+# git add "$1"
 read -p "请输入修改内容:" content
 git commit -m "$content"
 # git push -u origin master
